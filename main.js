02b09616ded929f9e09d2315d7b7c594e751e694
ts-webdev <dev.tanvirmahtab@gmail.com>
2025-08-27 15:17:32 +0600

    Complete cashout section functionality

--- main.js
+++ main.js
@@ -3,16 +3,24 @@
 document.getElementById('add-money').addEventListener('click', function(){
     document.getElementById('add-money-section').style.display = 'block'
     document.getElementById('cashout-section').style.display = 'none'
+
+    // active button 
+    document.getElementById('add-money').classList.add('active-btn')
+    document.getElementById('cash-out').classList.remove('active-btn')
 })
 
 // Toggle  cashout Section
 document.getElementById('cash-out').addEventListener('click', function(){
     document.getElementById('cashout-section').style.display = 'block'
     document.getElementById('add-money-section').style.display = 'none'
+
+    // active button 
+    document.getElementById('cash-out').classList.add('active-btn')
+    document.getElementById('add-money').classList.remove('active-btn')
 })
 
 
-// Add Money
+// Add Money Function
 document.getElementById('add-money-btn').addEventListener('click', function (e) {
     e.preventDefault()
 
@@ -32,6 +40,36 @@ document.getElementById('add-money-btn').addEventListener('click', function (e) 
 
             document.getElementById('add-pin').value = ''
             document.getElementById('add-amount').value = ''
+        }
+        else {
+            alert('Wrong Pin')
+        }
+    }
+    else{
+        alert('Enter Your Password')
+    }
+})
+
+
+// Withdraw Money Function
+document.getElementById('withdraw-btn').addEventListener('click', function (e) {
+    e.preventDefault()
+
+    // get information
+    const cashoutAccountNumber = document.getElementById('cashout-number').value;
+    const cashoutAmount = parseInt(document.getElementById('cashout-amount').value);
+    const cashoutPin = document.getElementById('cashout-pin').value;
+
+    // Get current Amount
+    const currentAmount = parseInt(document.getElementById('current-amount').innerText);
+
+    // Add New Amount after Withdraw money
+    if  (!cashoutPin == false) {
+        if (cashoutPin === "1234") {
+            document.getElementById('current-amount').innerText = currentAmount - cashoutAmount;
+
+            document.getElementById('cashout-pin').value = ''
+            document.getElementById('cashout-amount').value = ''
         }
         else {
             alert('Wrong Pin')
